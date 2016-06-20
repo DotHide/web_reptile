49cc0679a14b6c1b069993ef4c542c01f3dc97b4
Jack Zhang <dothide@gmail.com>
2016-06-20 15:50:27 +0800

    Update Test

--- spec/reptile_spec.rb
+++ spec/reptile_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe Reptile do
+describe WebReptile do
   it 'has a version number' do
-    expect(Reptile::VERSION).not_to be nil
+    expect(WebReptile::VERSION).not_to be nil
   end
 
   it 'should load a page' do
-    Reptile.url("http://www.jd.com/allSort.aspx") do |reptile|
-      expect(reptile).not_to be nil
+    WebReptile.url("http://www.jd.com/allSort.aspx") do |web|
+      expect(web).not_to be nil
     end
   end
 end
